4850257174637648974ad4d133830219ef01b086
brianzhibo-design <brianzhibo@gmail.com>
2025-12-21 16:53:50 +0800

    fix: 清理数据库中的无效权限值 (TEAM_KUDOS, FUN_EVENTS)

---
+++ backend/prisma/migrations/20251221_cleanup_invalid_permissions/migration.sql
@@ -0,0 +1,7 @@
+-- 清理无效的权限值
+-- 移除 TEAM_KUDOS 和 FUN_EVENTS 等后端不支持的权限
+
+-- 从 permissions 数组中移除无效值
+UPDATE "workspace_users" 
+SET permissions = array_remove(array_remove(permissions, 'TEAM_KUDOS'), 'FUN_EVENTS')
+WHERE permissions @> ARRAY['TEAM_KUDOS'] OR permissions @> ARRAY['FUN_EVENTS'];
